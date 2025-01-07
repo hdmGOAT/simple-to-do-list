1c234074829f6ef43d264024c87200ec639eb5a1
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-07 16:41:30 +0800

    more work

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Item = () => {
   return (
-    <div className="aspect-square flex align-middle items-center justify-center bg-zinc-600 rounded-2xl">
-      <h1>NOTE</h1>
+    <div className="aspect-square flex align-middle items-center justify-center bg-zinc-600 rounded-2xl flex-col p-5 space-y-5">
+      <div className="flex flex-1 size-full bg-white border border-dashed rounded-2xl items-center justify-center">
+        top
+      </div>
+      <div className="flex flex-[4] size-full bg-white border border-dashed rounded-2xl items-center justify-center">
+        {" "}
+        bot
+      </div>
     </div>
   );
 };
